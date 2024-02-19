03db8b8efd16adf4d6f129e8e0d4330896da58b4
T3rminator2297 <francis2297@hotmail.com>
2024-02-19 13:43:42 -0500

    Création d'un service REST a l'aide de flask

--- Connexion.py
+++ Connexion.py
@@ -1,19 +1,31 @@
+from flask import Flask, jsonify, abort
 import oracledb
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-try:
-    connexion = oracledb.connect(
-        config_dir=os.path.join(script_dir, "wallet"),
-        user="admin",
-        password="Lebelfranck2297$",
-        dsn="franciscloud_high",
-        wallet_location=os.path.join(script_dir, "wallet"),
-        wallet_password="Lebelfranck2297$")
-    print("Connexion à la base de données réussie.")
-    connexion.close()
-except oracledb.DatabaseError as e:
-    print("Erreur lors de la connexion à la base de données:", e)
+app = Flask(__name__)
 
 
+@app.route('/connexion')
+def connexion():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    try:
+        connection = oracledb.connect(
+            config_dir=os.path.join(script_dir, "wallet"),
+            user="admin",
+            password="Lebelfranck2297$",
+            dsn="franciscloud_high",
+            wallet_location=os.path.join(script_dir, "wallet"),
+            wallet_password="Lebelfranck2297$")
+        print("Connexion à la base de données réussie.")
+        connection.close()
+        return jsonify({"message": "Connexion à la base de données réussie."})
+    except oracledb.DatabaseError as e:
+        error_message = "Erreur lors de la connexion à la base de données: " + str(e)
+        print(error_message)
+        abort(500, error_message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    connexion()
