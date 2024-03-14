ae142e4b3bae8a3aa00e9c89d4831a8e094764b8
XavierOuellette <137076419+XavierOuellette@users.noreply.github.com>
2024-03-14 09:07:20 -0400

    + Ajouter délai de 5h pour db + Fix routes

--- Permissions.py
+++ Permissions.py
@@ -19,21 +19,12 @@ def get_permissions(session_id):
 
 
 @app.route("/has_permission", methods=["GET"])
-def has_permission():
+def has_permission_route():
     session_id = request.args.get('session_id')
     permission = request.args.get('permission')
-    cursor = connection.cursor()
 
-    # Retourne les permissions de l'utilisateur
-    perm_query = (f"SELECT rp.permission_name"
-                  "FROM SESSIONS s"
-                  "JOIN USERS u ON s.user_id = u.user_id"
-                  "JOIN ROLES r on u.user_id = r.user_id"
-                  "JOIN ROLE_PERMISSIONS rp ON r.role_name = rp.role_name"
-                  f"WHERE s.session_id = '{session_id}' AND rp.permission_name = '{permission}'")
-    cursor.execute(perm_query)
-    perm = cursor.fetchone()
-    if perm is None or permission not in perm:
+    has_perm = has_permission(session_id, permission)
+    if has_perm is False:
         return jsonify({"response": "false"})
     return jsonify({"response": "true"})
 
